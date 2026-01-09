0ef5a8da7764d49048d57c56bf8aab7b918a2d43
srikanth1104 <srikanth555chinnu@gmail.com>
2026-01-09 20:43:23 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -73,4 +73,8 @@ app.post('/api/jobs/:id/run', (req, res) => {
   });
 });
 
-app.listen(5000, () => console.log("Backend running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
+});
